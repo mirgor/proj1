fabd3b4ffd75cce83583e0e555a4d9032adddf7a
Miroslav Botsula <botsula@gmail.com>
2017-09-15 11:42:45 +0300

    test3

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
+    sass = require('gulp-sass');
+var server = require('gulp-server-livereload');
+
+gulp.task('webserver', function () {
+    gulp.src('./')
+        .pipe(server({
+            livereload: {
+                enable: true,
+                filter: function (filePath, cb) {
+                    cb(!(/node_modules/.test(filePath)));
+                },
+              },
+            defaultFile: 'app/index.html',
+            fallback: 'app/index.html',
+            open: true
+        }));
+});
 
 gulp.task('sass', function(){
 return gulp.src('sass/*.sass')
-.pipe(sass())
-.pipe(gulp.dest('app/css/'))
+    .pipe(sass())
+    .pipe(gulp.dest('app/css/'))
 });
 
 gulp.task('watch', function(){
-gulp.watch('sass/*.sass', ['sass']);
+    gulp.watch('sass/*.sass', ['sass']);
 });
+
 gulp.task('default', ['watch']);
